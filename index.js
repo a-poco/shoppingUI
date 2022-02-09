fd58e1db2b2e9ff9f61c865ac6224aef4d3adf86
Cristina Salazar <cristinasalazarm1@gmail.com>
2022-02-09 17:41:26 +0100

    fetch products and display

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const path = require('path');
+const fetch = require('node-fetch')
 const { engine } = require('express-handlebars');
 
 const app = express();
@@ -11,7 +12,33 @@ app.set('views', './views');
 
 app.get('/', (req, res) => {
     res.render('home')
+});
+
+const getProducts = async () => {
+    const response = await fetch('http://localhost:3001/api/products');
+    const data = await response.json();
+    const promises = data.map(async (id) => {
+        const response = await fetch(`http://localhost:3001/api/products/${id}`)
+        const detail = await response.json()
+        return detail;
+    });
+    const result = await Promise.all(promises);
+    return result;
+}
+
+app.get('/products', async (req, res) => {
+    const products = await getProducts();
+    res.render('products', {products})
+});
+
+app.get('/products/:id', async (req, res) => {
+    const id = req.params.id;
+    const products = await getProducts();
+    const product = products.find(product => product.id === id);
+    res.render('productDetails', { products, product })
 })
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Other middleware follows below...
 app.listen(3000);
